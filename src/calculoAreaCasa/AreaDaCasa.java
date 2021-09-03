a2adaf2a1e756f66e73a2adb88c18bebf5acf68d
VINICIO DE JESUS SILVA <21276440@CFP127ALUNO.LOCAL>
2021-09-03 16:51:32 -0300

    o que foi feito até o momento

--- src/calculoAreaCasa/AreaDaCasa.java
+++ src/calculoAreaCasa/AreaDaCasa.java
@@ -1,11 +1,22 @@
 package calculoAreaCasa;
 
+import java.util.Scanner;
+
 public class AreaDaCasa {
 
 	public static void main(String[] args) {
 		
-		/*Inicio Projeto da casa*/
-
+		Scanner leitor = new Scanner (System.in);
+		
+		
+		
+		System.out.println("---------------------------");
+		System.out.println("Calculo da área de uma casa");
+		System.out.println("---------------------------");
+		/*while (resposta.equals("S")){
+			
+		}*/
+		
 	}
 
 }
